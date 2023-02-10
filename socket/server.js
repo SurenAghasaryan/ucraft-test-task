5b061659d75d67fe38c2962bc6160fab2201ea14
suren <suren.aghasaryan@gmail.com>
2023-02-10 18:12:44 +0400

    Create simple socket

---
+++ socket/server.js
@@ -0,0 +1,23 @@
+const WebSocket = require('ws');
+
+const server = new WebSocket.Server({
+  port: 5000
+});
+
+server.on('connection', (socket) => {
+  console.log('Client connected');
+
+  socket.on('message', (message) => {
+    console.log(`Received message: ${message}`);
+
+    server.clients.forEach((client) => {
+      if (client !== socket && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  socket.on('close', () => {
+    console.log('Client disconnected');
+  });
+});
