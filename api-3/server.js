7dd82d0ad5beb171842c747f5531301a02e6ebc7
suren <suren.aghasaryan@gmail.com>
2023-02-13 04:56:30 +0400

    Add api-2 and api-3

---
+++ api-3/server.js
@@ -0,0 +1,58 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const { Kafka } = require("kafkajs");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.use(
+  cors({
+    origin: "http://localhost:3001",
+  })
+);
+
+const kafka = new Kafka({
+  clientId: "api-3",
+  brokers: ["kafka:9092"],
+});
+
+const producer = kafka.producer({
+  retries: 5,
+  retry: {
+    maxRetryTime: 10000,
+    initialRetryTime: 300,
+    factor: 2,
+  },
+});
+
+const sendMessage = async (message) => {
+  await producer.connect();
+  await producer.send({
+    topic: "update-score",
+    messages: [{ value: message }],
+  });
+  await producer.disconnect();
+};
+
+app.post("/messages", async (req, res) => {
+  const { message } = req.body;
+
+  console.log(message);
+  try {
+    await sendMessage(message);
+    res.status(206).send("done");
+  } catch(err) {
+    console.log(err)
+  }
+});
+
+app.get("/", (req, res) => {
+  res.send("Connected to api-3");
+});
+
+app.listen(4003, () => {
+  console.log(`Server is running on port ${4003}.`);
+});
